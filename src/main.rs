530ffc1f3504d2813e9cadfd577b9c4e7d394edf
jov-1024 <90773264+jov-1024@users.noreply.github.com>
2022-07-12 13:49:42 +0530

    Init commit, added path inputting functionality

---
+++ src/main.rs
@@ -0,0 +1,20 @@
+// use std::fs::File;
+use std::io;
+use std::path::PathBuf;
+fn main() {
+    let mut path_buf = &mut PathBuf::new();
+    let path_dark = input_path("Enter Path to Dark Wallpaper", path_buf);
+    println!("{:?}", path_dark);
+}
+
+fn input_path<'a>(question: &str, path_obj: &'a mut PathBuf) -> &'a PathBuf {
+    println!("{}", question);
+    let mut buf = String::new();
+    io::stdin()
+        .read_line(&mut buf)
+        .expect("Couldnt properly read input");
+    buf = buf.trim().to_string();
+    path_obj.clear();
+    *path_obj = PathBuf::from(buf);
+    path_obj
+}
