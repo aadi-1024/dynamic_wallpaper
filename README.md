41fe7c62891d0afd6ee957f453338e0732c4612f
jov-1024 <90773264+jov-1024@users.noreply.github.com>
2022-07-13 13:42:52 +0530

    Added functionality to verify whether provided path is valid and leads to a supported image

--- README.md
+++ README.md
@@ -7,3 +7,8 @@ This is supposed to be purely a learning project. There probably are far better 
 It's not complete either yet, so yeah.
 
 Coded in Rust.
+
+TODO:
+- Finish it
+- For now it uses file extensions to check filetype. Make it usable for image files without any extension.
+- Add a gtk4 interface
--- src/main.rs
+++ src/main.rs
@@ -4,9 +4,10 @@ use std::path::PathBuf;
 fn main() {
     let path_buf = &mut PathBuf::new();
     let path_dark = input_path("Enter Path to Dark Wallpaper", path_buf);
-    println!("{:?}", path_dark);
+    dbg!(verify_image(path_dark)); 
 }
 
+//takes input path and returns a PathBuf
 fn input_path<'a>(question: &str, path_obj: &'a mut PathBuf) -> &'a PathBuf {
     println!("{}", question);
     let mut buf = String::new();
@@ -17,4 +18,27 @@ fn input_path<'a>(question: &str, path_obj: &'a mut PathBuf) -> &'a PathBuf {
     path_obj.clear();
     *path_obj = PathBuf::from(buf);
     path_obj
+}
+
+//checks whether the provided path is valid and leads to a jpg, jpeg or png
+fn verify_image(path_obj: &PathBuf) -> bool { 
+    if !path_obj.exists() {
+        false
+    } else {
+        if let Some(t) = path_obj.extension() {
+            dbg!(t);
+            if let Some(x) = t.to_str() {
+                dbg!(x);
+                match x {
+                    "png" | "jpg" | "jpeg" => true,
+                    _ => false
+                }
+            }
+            else {
+                false
+            }
+        } else {
+            false
+        }
+    }
 }
